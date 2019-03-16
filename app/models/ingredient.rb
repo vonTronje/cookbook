d1460439459be5ef97f54ffb9e39a64fa39cb256
Hagen Mahnke <hagen.mahnke@kaeuferportal.de>
2019-03-16 21:02:26 +0100

    Add validations to Ingredient and test them
    Validations match constraints set on database level.
    - must have a name
    - default_unit must be included in type definition for unit or nil

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true
+  validates :default_unit,
+            inclusion: { in: %w(mg g kg l EL TL Msp Bund Packung) },
+            allow_nil: true
 end
--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "saves an ingredient" do
+    ingredient = Ingredient.new(name: 'Test Ingredient')
+    assert ingredient.save
+  end
+
+  test "does not save a ingredient without a name" do
+    ingredient = Ingredient.new
+    assert_not ingredient.save
+  end
+
+  test "saves an ingredient with a valid default_unit" do
+    ingredient = Ingredient.new(name: 'Test Ingredient', default_unit: 'kg')
+    assert ingredient.save
+  end
+
+  test "does not save an ingredient with an invalid default_unit" do
+    ingredient = Ingredient.new(name: 'Test Ingredient', default_unit: 'cm')
+    assert_not ingredient.save
+  end
 end
