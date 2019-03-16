a41037aeedbe98bc4e8adbd73cbf1d55231e03cc
Hagen Mahnke <hagen.mahnke@kaeuferportal.de>
2019-03-16 21:26:02 +0100

    Add validations to Recipe and test them
    Validations match constraints set on database level.
    - must have a name
    - must have instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
 
+  validates :name, presence: true
+  validates :instructions, presence: true
+
   def add_ingredient(ingredient:, quantity:, unit:)
     recipe_ingredient = RecipeIngredient.new(ingredient: ingredient,
                                              quantity: quantity,
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "saves a recipe" do
+    recipe = Recipe.new(name: 'test', instructions: 'test')
+    assert recipe.save
+  end
+
+  test "does not save recipe without a name" do
+    recipe = Recipe.new(instructions: 'test')
+    assert_not recipe.save
+  end
+
+  test "does not save recipe without instructions" do
+    recipe = Recipe.new(name: 'test')
+    assert_not recipe.save
+  end
+
 end
