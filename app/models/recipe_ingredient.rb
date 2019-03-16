6abfd0fd64c4d8c8f68c3c5629079003efa44c84
Hagen Mahnke <hagen.mahnke@kaeuferportal.de>
2019-03-16 21:28:36 +0100

    Add validations to RecipeIngredient and test them
    Validations match constraints set on database level.
    - must have a quantity
    - must have a unit
    - unit must be included in type definition for unit
    
    Add fixtures for Recipe, Ingredient, RecipeIngredients

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,10 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :quantity, presence: true
+  validates :unit,
+            presence: true,
+            inclusion: { in: %w(mg g kg l EL TL Msp Bund Packung) }
+
 end
--- test/models/recipe_ingredient_test.rb
+++ test/models/recipe_ingredient_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class RecipeIngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "saves a recipe_ingredient" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:milk)
+    recipe_ingredient = RecipeIngredient.new(recipe: recipe,
+                                             ingredient: ingredient,
+                                             quantity: 1,
+                                             unit: 'l')
+    assert recipe_ingredient.save
+  end
+
+  test "does not save a recipe_ingredient without a recipe" do
+    ingredient = ingredients(:milk)
+    recipe_ingredient =
+      RecipeIngredient.new(ingredient: ingredient, quantity: 1, unit: 'l')
+    assert_not recipe_ingredient.save
+  end
+
+  test "does not save a recipe_ingredient without an ingredient" do
+    recipe = recipes(:pancakes)
+    recipe_ingredient =
+      RecipeIngredient.new(recipe: recipe, quantity: 1, unit: 'l')
+    assert_not recipe_ingredient.save
+  end
+
+  test "does not save a recipe_ingredient without a quantity" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:milk)
+    recipe_ingredient =
+      RecipeIngredient.new(recipe: recipe, ingredient: ingredient, unit: 'l')
+    assert_not recipe_ingredient.save
+  end
+
+  test "does not save a recipe_ingredient without a unit" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:milk)
+    recipe_ingredient =
+      RecipeIngredient.new(recipe: recipe, ingredient: ingredient, quantity: 1)
+    assert_not recipe_ingredient.save
+  end
+
+  test "does not save an recipe_ingredient with an invalid unit" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:milk)
+    recipe_ingredient = RecipeIngredient.new(recipe: recipe,
+                                             ingredient: ingredient,
+                                             quantity: 1,
+                                             unit: 'cm')
+    assert_not recipe_ingredient.save
+  end
 end
