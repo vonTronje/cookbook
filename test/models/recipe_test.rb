91255222c5ee3287a1439305d17106396a73e290
Hagen Mahnke <hagen.mahnke@kaeuferportal.de>
2019-03-16 21:42:07 +0100

    Add tests for adding an ingredient to a recipe

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -16,4 +16,34 @@ class RecipeTest < ActiveSupport::TestCase
     assert_not recipe.save
   end
 
+  test "adds an ingredient" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:flour)
+    recipe.add_ingredient(ingredient: ingredient, quantity: 400, unit: 'g')
+    assert_equal recipe.recipe_ingredients.first.ingredient_id, ingredient.id
+  end
+
+  test "does not add an ingredient without ingredient" do
+    recipe = recipes(:pancakes)
+    assert_raises ArgumentError do
+      recipe.add_ingredient(quantity: 400, unit: 'g')
+    end
+  end
+
+  test "does not add an ingredient without quantity" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:flour)
+    assert_raises ArgumentError do
+      recipe.add_ingredient(ingredient: ingredient, unit: 'g')
+    end
+  end
+
+  test "does not add an ingredient without unit" do
+    recipe = recipes(:pancakes)
+    ingredient = ingredients(:flour)
+    assert_raises ArgumentError do
+      recipe.add_ingredient(ingredient: ingredient, quantity: 400)
+    end
+  end
+
 end
